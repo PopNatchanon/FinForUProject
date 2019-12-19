ee28b6e749edd57686f2e3368abf3a926e7b478c
Methawee2822538 <58871291+Methawee2822538@users.noreply.github.com>
2019-12-19 16:19:48 +0700

    dataService

--- DetailScreen.js
+++ DetailScreen.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  Text,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+// import SwiperFlatList from 'react-native-swiper-flatlist';
+// import axios from 'axios';
+// import NumberFormat from 'react-number-format';
+import Icons from 'react-native-vector-icons/FontAwesome5';
+import IconsFeather from 'react-native-vector-icons/Feather';
+import styles from './StylesMainScreen'
 
 export default class DetailScreen extends Component {
   constructor(props) {
@@ -11,11 +25,34 @@ export default class DetailScreen extends Component {
   render() {
     return (
       <View>
-        <Text> DetailScreen </Text>
+        <AppBar navigation={this.props.navigation}/>
       </View>
     );
   }
 }
 
+export class AppBar extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          text: '',
+      };
+  }
 
+  render() {
+      return (
+          <View style={styles.Appbar}>
+              <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+                  <Icons RightItem name="arrow-left" size={20} style={{ marginTop: 5, }} />
+              </TouchableOpacity>
+              <TextInput style={styles.TextInput}
+                  placeholder="ค้นหาสินค้า/ร้านค้า"
+                  onChangeText={(text) => this.state({ text })}
+              ></TextInput>
+              <IconsFeather RightItem name="filter" size={20} style={{ marginTop: 5, }} />
+              <Icons RightItem name="ellipsis-h" size={20} style={{ marginTop: 5, }} />
+          </View>
+      );
+  }
+}
 
---
+++ database/DataService.php
@@ -0,0 +1,59 @@
+<?php
+include 'DBConfig.php';
+ 
+// Create connection
+$conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
+
+
+if ($conn->connect_error) {
+ 
+ die("Connection failed: " . $conn->connect_error);
+}
+
+
+$Stype=$_GET['type'];
+
+if($Stype=='slide'){
+    $sql = "SELECT image , image_path FROM slide_copy limit 5";
+}else if($Stype=='type'){
+    $sql = "SELECT name ,  image_menu  FROM type ";
+}else if($Stype=='sale') {
+    $sql = "SELECT image , name , sale_price FROM product LIMIT 6";
+}else if($Stype=='store') {
+    $sql = "SELECT image  , name FROM store  LIMIT 6";
+}else if($Stype=='product') {
+    $sql = "SELECT id_product , name , image , full_price , sale_price , discount  FROM product Order by date DESC  LIMIT 6";
+}else if( $Stype == 'todayproduct' ){
+    $sql = "SELECT id_product , name , image , full_price , sale_price , discount FROM product limit 20";
+}else if($Stype=='brand'){
+    $sql = "SELECT image , image_path  FROM slide WHERE type_icon = 'promotions' AND status_active = '1'  limit 6 ";
+}else if($Stype=='foryou') {
+    $sql = "SELECT image ,name ,sale_price, image_path FROM product limit 10 ";
+}else if( $Stype == 'categorylist' ) {
+    $sql = "SELECT name , image_head  FROM type ";
+}else if($Stype == 'categoryproduct' ) {
+    $Sproduct = $_GET['product'];
+    $sql = "SELECT id_product , name , image , full_price , sale_price , discount , type FROM product WHERE type = '$Sproduct' limit 6";
+}else if($Stype=='Confidential_PRO'){
+    $sql = "SELECT image , image_path  FROM slide WHERE type_icon = 'Confidential_PRO' AND status_active = '1'  limit 3 ";
+}
+ 
+$result = $conn->query($sql);
+ 
+if ($result->num_rows >0) {
+ 
+ 
+ while($row[] = $result->fetch_assoc()) {
+ 
+ $tem = $row;
+ 
+ $json = json_encode($tem, JSON_UNESCAPED_UNICODE);
+
+ }
+ 
+} else {
+ echo "No Results Found.";
+}
+ echo $json;
+$conn->close();
+?>

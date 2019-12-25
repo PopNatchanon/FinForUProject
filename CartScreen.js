57c981834f2207e0532f49480286630d8b282f41
Methawee2822538 <58871291+Methawee2822538@users.noreply.github.com>
2019-12-25 11:02:40 +0700

    up รถเข็น

--- CartScreen.js
+++ CartScreen.js
@@ -15,7 +15,7 @@ import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Icons from 'react-native-vector-icons/FontAwesome';
 import IconsFeather from 'react-native-vector-icons/Feather';
 import IconAntDesign from 'react-native-vector-icons/AntDesign';
-import IconEntypo from 'react-native-vector-icons/Entypo';
+import IconFoundation from 'react-native-vector-icons/Foundation';
 import styles_A from './stylesCartScreen';
 import styles from './StylesDetailScreen';
 import NumberFormat from 'react-number-format';
@@ -213,8 +213,27 @@ export class Buy_bar extends Component {
     render() {
         return (
             <View style={styles_A.Bar}>
-                <Text> CartScreen </Text>
-                <View style={styles_A.Bar_Code} > Code</View>
+                <View style={styles_A.Bar_Code} >
+                    <IconFoundation name='burst' size={30} />
+                    <Text>โค้ดส่วนลด</Text>
+                    <View style={styles_A.Bar_Code_Box}></View>
+                    <View style={styles_A.Bar_Code_Box_Text}>
+                        <Text style={styles_A.Bar_Code_Text}>ใช้โค้ด</Text>
+                    </View>
+                </View>
+                <View style={styles_A.Bar_Buy}>
+                    <View style={styles_A.Bar_Buy_select}>
+                        <View style={styles_A.Boxselect}></View>
+                        <Text> เลือกทั้งหมด</Text>
+                    </View>
+                    <View style={styles_A.Bar_Buy_price}>
+                        <Text>รวมทั้งหมด</Text>
+                        <Text> ฿10,000</Text>
+                    </View>
+                    <View style={styles_A.BOX_Buy}>
+                        <Text style={styles_A.BOX_Buy_Text}>ชำระเงิน</Text>
+                    </View>
+                </View>
             </View>
         );
     }
--- stylesCartScreen.js
+++ stylesCartScreen.js
@@ -3,8 +3,8 @@ import { StyleSheet, Dimensions } from 'react-native';
 export const { width, height } = Dimensions.get('window');
 
 export default StyleSheet.create({
-    SafeAreaView:{
-        flex:1,
+    SafeAreaView: {
+        flex: 1,
     },
     Appbar: {
         backgroundColor: '#0A55A6',
@@ -47,14 +47,76 @@ export default StyleSheet.create({
         backgroundColor: '#FFFFFF',
         borderColor: '#ECECEC',
         borderWidth: 1,
-        height:'auto',
+        height: 110,
         width,
 
     },
-    Bar_Code:{
-        height:50,
+    Bar_Code: {
+        height: 50,
         backgroundColor: '#FFFFFF',
         borderColor: '#ECECEC',
         borderWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingTop: 5,
+
     },
-})
+    Bar_Code_Box: {
+        width: 200,
+        height: 30,
+        borderColor: '#ECECEC',
+        borderWidth: 1,
+        paddingTop: 5,
+    },
+    Bar_Code_Box_Text: {
+        width: 60,
+        height: 35,
+        borderColor: '#ECECEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingTop: 5,
+        backgroundColor: '#0A55A6',
+    },
+    Bar_Code_Text: {
+        textAlign: 'center',
+        color: '#FFFF',
+    },
+    Bar_Buy: {
+        height: 60,
+        backgroundColor: '#FFFFFF',
+        borderColor: '#ECECEC',
+        borderWidth: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingTop:10,
+    },
+    Bar_Buy_select: {
+        height: 40,
+        flexDirection: 'row',
+    },
+    Boxselect: {
+        borderColor: 'black',
+        borderWidth: 1,
+        width: 15,
+        height: 15,
+        marginTop:5,
+
+    },
+    Bar_Buy_price: {
+        height: 40,
+        flexDirection: 'row',
+    },
+    BOX_Buy: {
+        width: 80,
+        height: 35,
+        borderColor: '#ECECEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingTop: 5,
+        backgroundColor: '#0A55A6',
+    },
+    BOX_Buy_Text: {
+        textAlign: 'center',
+        color: '#FFFF',
+    },
+}) 

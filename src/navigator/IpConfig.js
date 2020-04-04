5e43c89579fec820f593823c2fca157342054724
PopNatchanon <58871246+PopNatchanon@users.noreply.github.com>
2020-04-04 09:32:29 +0700

    Update IpConfig.js

--- src/navigator/IpConfig.js
+++ src/navigator/IpConfig.js
@@ -1,6 +1,6 @@
 // export const ip = 'http://192.168.0.131';
-export const ip = 'http://192.168.0.132';
-// export const ip = 'http://10.100.200.152';
+// export const ip = 'http://192.168.0.132';
+export const ip = 'http://10.100.200.152';
 // export const ip = 'http://192.168.42.252';
 // export const ip = 'http://192.168.1.36';
 

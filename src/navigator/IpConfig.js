aa1b5105239affb026d66c81cc2e5307f540a002
Methawee2822538 <58871291+Methawee2822538@users.noreply.github.com>
2020-04-21 17:07:34 +0700

    API ExclusiveScreen

--- src/navigator/IpConfig.js
+++ src/navigator/IpConfig.js
@@ -1,8 +1,6 @@
-export const ip = 'http://192.168.0.131';
+// export const ip = 'http://192.168.0.131';
 // export const ip = 'http://192.168.0.132';
 // export const ip = 'http://10.100.200.173';
-// export const ip = 'http://192.168.43.141';
+export const ip = 'http://192.168.43.166';
 // export const ip = 'http://192.168.1.41';
-
-
 export const finip = 'http://www.mmnie.live'
